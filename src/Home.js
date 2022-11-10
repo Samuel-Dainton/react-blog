718b49b12fc773120c1ef6d4f3b2c54bf8a3980a
Sam Dainton <dexterdainton@gmail.com>
2022-11-10 14:34:07 +0000

    Props to pass data between home and blog list

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
 
@@ -59,12 +60,7 @@ const Home = () => {
 
             <button onClick={changeAge}>Change Age to 35</button>
 
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                </div>
-            ))}
+             <BlogList blogs={blogs} title="All Blogs" />
         </div>
     );
 }

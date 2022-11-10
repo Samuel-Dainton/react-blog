bf440d2191512fb8bafcb35a0623dd197d67a4d8
Sam Dainton <dexterdainton@gmail.com>
2022-11-10 13:11:16 +0000

    css and comments to show how to inline style

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,15 @@ const Navbar = () => {
             <h1>The React Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/">New Blog</a>
+                {/**
+                 * Inline style needs a dynamic value {inside curly braces} and an {object} inside of that.
+                 * The keys since we're typing in jsx need to be camel case, not like css which would be background-color.
+                 */}
+                <a href="/" style={{
+                    color: "white",
+                    backgroundColor: "#f1356d",
+                    borderRadius: "8px"
+                }}>New Blog</a>
             </div>
         </nav>
      );

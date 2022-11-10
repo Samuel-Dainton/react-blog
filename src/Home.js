a0c84d9ee8f0ac6da9a22cd07cc7619a35a29314
Sam Dainton <dexterdainton@gmail.com>
2022-11-10 15:00:41 +0000

    useEffect trigger

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
 
-    {/**
+    /**
      * With the below example comment, the {name} would not be updated in the browser because it is not reactive.
      * In order for it to work we need to make the value reactive. We do this by using a 'hook'. This hook is called useState.
      * useState is imported from react as above and then we make the value reactive below.
      * useState is the value, with the value of Sam
      * Typically you use 'set' and what you are changing to name the call
-     */}
+     */
 
     // let name = "Sam";
     const [name, setName] = useState('Sam');
@@ -20,18 +20,26 @@ const Home = () => {
         { title: "I love it", body: "lorem ipsum...", author: 'Dan', id: 2 },
         { title: "Web dev top tips", body: "lorem ipsum...", author: 'Sam', id: 3 }
     ]);
-
     
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
         }
     
+    /* useEffect triggers every time the page is rendered with new information. Useful for fetching data.
+    * by adding an empty array as a second param, the useEffect will only trigger on the first page load. 
+    * by adding a value to the array, only if that value is changed will the useEffect trigger.
+    */
 
-    {/**
+    useEffect(()=>{
+        console.log('use effect triggered');
+        console.log(blogs);
+    }, [name]);
+
+    /**
     * Where you reference a function, it automatically gains the e (event) obeject as its first param
     * You don't have to use this, the e's can be left blank
-    */}
+    */
 
     const handleClick = () => {
         console.log("Hello from the console.");

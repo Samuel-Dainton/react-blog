91ca9dd76575c87ff3a272178449980ea905e782
Sam Dainton <dexterdainton@gmail.com>
2022-11-10 17:37:45 +0000

    react router links to speed up navigation

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,20 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>The React Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
+                <Link to="/">Home</Link>
                 {/**
                  * Inline style needs a dynamic value {inside curly braces} and an {object} inside of that.
                  * The keys since we're typing in jsx need to be camel case, not like css which would be background-color.
                  */}
-                <a href="/create" style={{
+                <Link to="/create" style={{
                     color: "white",
                     backgroundColor: "#f1356d",
                     borderRadius: "8px"
-                }}>New Blog</a>
+                }}>New Blog</Link>
             </div>
         </nav>
      );

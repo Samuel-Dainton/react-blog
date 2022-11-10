b3578e6a86dcca016dc35e713252e8e1e093fb3d
Sam Dainton <dexterdainton@gmail.com>
2022-11-10 19:50:29 +0000

    Submit form working

--- src/Create.js
+++ src/Create.js
@@ -2,14 +2,31 @@ import { useState } from "react";
 
 
 const Create = () => {
-    const [title, setTitle] = useState('')
-    const [body, setBody] = useState('')
-    const [author, setAuthor] = useState('Sam')
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Sam');
+    const [isPending, setIsPending] =useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const blog = {title, body, author};
+
+        setIsPending(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(()=>{
+            console.log('newblogadded')
+            setIsPending(false)
+        })
+    }
 
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label >Blog title:</label>
                 <input
                     type="text"
@@ -33,7 +50,8 @@ const Create = () => {
                     <option value="Dan">Dan</option>
                     <option value="Mel">Mel</option>
                 </select>
-                <button>Add Blog</button>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabled>Submitting...</button>}
                 <p>{title}</p>
                 <p>{body}</p>
             </form>
